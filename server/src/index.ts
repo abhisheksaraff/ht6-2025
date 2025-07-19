67a6bfa99c2d18f0c9783b8a1d042811426ec3c0
a6hi <saraffabhishek@outlook.com>
2025-07-18 22:43:52 -0400

    setup index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,45 @@
 import { Hono } from "hono";
+import { UUID } from "mongodb";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
+// env.VARIABLE;
+// UUID v7
+// ttl 10 * 60
+// {
+//   content: ""
+//   role: "" // user or model or system
+// }
 
-app.get("/message", (c) => {
+// return
+// {
+//   id: "",
+//   ttl: ttl,
+// }
+app.post("/api/content", (c) => {
+  return c.text("Hello Hono!");
+});
+
+// UUID v7
+// reset ttl to = 10mins
+//{
+//   id: "",
+//   content: "" optional
+// } res.status(200)
+
+// return
+// {
+//   id: "",
+//   ttl: ttl,
+// } res.status(200)
+app.put("/api/content", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.options("/api/content", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.post("/api/generate", (c) => {
   return c.text("Hello Hono!");
 });
 
