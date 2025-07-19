7664e3b1dceaaca4929e59fd1f7c416ed0e752de
longlodw <longlodw@gmail.com>
2025-07-19 02:26:38 -0400

    remove unused file

--- server/src/mongo.ts
+++
@@ -1,6 +0,0 @@
-export interface IMongoClient {
-  connect: (connString: string) => Promise<void>;
-  disconnect: () => Promise<void>;
-  store: <T>(name: string, document: T, ttl: number) => Promise<void>;
-  load: <T>(name: string) => Promise<T | null>;
-}
