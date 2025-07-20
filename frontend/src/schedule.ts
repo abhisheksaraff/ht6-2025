00a66ff6d83d3a31c818bdc968c56e1cbb14875f
longlodw <longlodw@gmail.com>
2025-07-19 20:13:36 -0400

    schedule

---
+++ frontend/src/schedule.ts
@@ -0,0 +1,30 @@
+export interface ISchedule {
+  add(id: string, t: number, task: () => void): void;
+  remove(id: string): void;
+}
+
+export class Schedule implements ISchedule {
+  private tasks: Map<string, number> = new Map();
+
+  add(id: string, t: number, task: () => void): void {
+    if (this.tasks.has(id)) {
+      throw new Error(`Task with id ${id} already exists.`);
+    }
+    const timeOutId = setTimeout(() => {
+      task();
+      this.tasks.delete(id);
+    }, t);
+    this.tasks.set(id, timeOutId);
+  }
+
+  remove(id: string): void {
+    if (!this.tasks.has(id)) {
+      throw new Error(`Task with id ${id} does not exist.`);
+    }
+    const timeOutId = this.tasks.get(id);
+    if (timeOutId !== undefined) {
+      clearTimeout(timeOutId);
+      this.tasks.delete(id);
+    }
+  }
+}
